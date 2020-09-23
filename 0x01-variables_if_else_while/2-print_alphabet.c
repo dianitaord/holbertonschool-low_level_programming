34b950f2f154178f14af915257136c41dedbceab
dianitaord <dianitaord@gmail.com>
2020-09-22 21:01:42 -0500

    2. I sometimes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main: Prints the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	int alphabets;
+
+	for (alphabets = 97 ; alphabets <= 122 ; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	putchar(10);
+	return (0);
+}
