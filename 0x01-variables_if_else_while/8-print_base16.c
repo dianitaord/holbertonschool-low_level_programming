97485cf81f1daf4c26fdbea705cefe4c0ad02608
dianitaord <dianitaord@gmail.com>
2020-09-23 01:01:44 -0500

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int base10;
+	int letra;
+
+	for (base10 = 48; base10 <= 57; base10++)
+	{
+		putchar(base10);
+	}
+	for (letra = 97; letra <=102; letra++)
+	{
+		putchar(letra);
+	}
+	putchar(10);
+	return (0);
+}
